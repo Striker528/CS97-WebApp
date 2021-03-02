6f05d2cd99e72c9716893d717274fa42d42b3e44
Striker528 <austyn.j.adams@gmail.com>
2021-03-02 09:46:37 -0800

    Finished Search and this is the bare skeleton of the final

--- Examples and Notes/CodeSandbox-StarWars-Working/davids-working-web-app/server.js
+++ Examples and Notes/CodeSandbox-StarWars-Working/davids-working-web-app/server.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser"); // import body parser library
 const MongoClient = require("mongodb").MongoClient; // import mongodb library
 const app = express();
 
+var assert = require('assert');
+
 // neeed to have bodyparer before CRUD handlers
 // app.use(bodyParser.urlencoded({extended: true}))
 // urlendcoded tells bodyparser to extract data from the form element and put them in the body of the req object
@@ -57,6 +59,11 @@ MongoClient.connect(uri, { useUnifiedTopology: true }) // removing the deprecati
     });
 
     app.get("/", (req, res) => {
+      
+      // Search Stuff
+
+      // *****************************************************
+
       // res.sendFile(__dirname + '/index.html')
 
       // find method returns a cursor
@@ -77,9 +84,62 @@ MongoClient.connect(uri, { useUnifiedTopology: true }) // removing the deprecati
       // res.render('index.ejs', {})
     });
 
-    app.post("/events", (req, res) => {
+    //app.get('/get-data', (req, res) => {
+      //var resultArray = [];
+      //var cursor = db.collection("events").find();
+      //cursor.forEach(function(doc, err) {
+        //assert.(null, err);
+        //resultArray.push(doc);
+      //}, function() {
+        //db.close();
+        //res.render('index', {items: resultArray});
+      //});
+    //});
+
+    app.get('/search-data', (req, res) => {
+
+      db.find({Day: req.query.search[0], Day: req.query.search[1]}, function(err, data){
+        if(err){
+          res.send(err.message);
+        }
+        else{
+          res.send(data);
+        }
+      });
+      
+
+
+      //var noMatch = null;
+    //if(req.query.search) {
+        //const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        // Get all campgrounds from DB
+        //Campground.find({name: regex}, function(err, allCampgrounds){
+           //if(err){
+               //console.log(err);
+           //} else {
+              //if(allCampgrounds.length < 1) {
+                  //noMatch = "No campgrounds match that query, please try again.";
+              //}
+              //res.render("campgrounds/index",{campgrounds:allCampgrounds, noMatch: noMatch});
+           //}
+        //});
+    //} else {
+        // Get all campgrounds from DB
+        //Campground.find({}, function(err, allCampgrounds){
+           //if(err){
+               //console.log(err);
+           //} else {
+              //res.render("campgrounds/index",{campgrounds:allCampgrounds, noMatch: noMatch});
+           //}
+        //});
+    //}
+    });
+
+    //app.post("/events", (req, res) => {
+    app.post("/post-events", (req, res) => {
       eventsCollection
         .insertOne(req.body)
+        //what we submit will be combined together into 1 req.body
         .then((result) => {
           res.redirect("/");
         })
@@ -92,3 +152,7 @@ MongoClient.connect(uri, { useUnifiedTopology: true }) // removing the deprecati
   })
 
   .catch(console.error);
+
+  //function escapeRegex(text) {
+    //return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+//};
